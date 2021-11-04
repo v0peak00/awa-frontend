43a87eb1dc24420ca31f5b0334d465fb3087f140
koulu <koulu@debian.localdomain>
2021-11-04 12:29:31 +0200

    sketching menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {useStates} from './StatesProvider';
 
 export default function Menu() {
+    const {activeRestaurant} = useStates();
+
     return (
         <div>
+            Menu - {activeRestaurant}
         </div>
     )
 }
