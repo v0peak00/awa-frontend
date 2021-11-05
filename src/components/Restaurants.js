9378e700a3c3b83a622a324fa71a4a2a8f2d0e6b
Aki Peltokangas <v0peak00@students.oamk.fi>
2021-11-05 12:38:48 +0200

    Restaurants with dummy data
    added restaurant dummy data file: data.restaurants.js
    
    added folder for the images.
    
    Changes to restaurant component.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import styles from './Restaurants.module.css';
+import { restaurantData } from '../data.restaurants';
+
+
 
 export default function Restaurants() {
     return (
-        <div>
-           Restaurants 
+        <div className={styles.container}>
+               {restaurantData.map((data, key) => {
+                   return (
+                    <div className={styles.box}>
+                       <div key={key}>
+                           <img src={data.image} alt=""/>
+                                <div className={styles.title}>
+                                <a href=""> {data.name}</a>   
+                                </div>
+                           <div className={styles.restInfo}>
+                                 <div>
+                                    {data.address}
+                                </div>
+                                <div>
+                                    Avoinna: {data.openHrs}
+                                </div>
+                                <div>
+                                    Hinta: {data.price}
+                                </div>
+                                <div>
+                                    Tyyli: {data.type}
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                   );
+                     
+               })}
+             
         </div>
     )
 }
---
+++ src/data.restaurants.js
@@ -0,0 +1,48 @@
+export const restaurantData = [
+    {
+        id: 1,
+        name: "Bandidos",
+        address: "Puistokatu 31",
+        openHrs: "11:00 - 22:00",
+        image: "./images/pic1.png",
+        type: "Casual",
+        price: "$$"
+
+    },
+    {
+        id: 2,
+        name: "Malik's",
+        address: "Hallituskatu 3",
+        openHrs: "14:00 - 22:00",
+        image: "./images/pic2.png",
+        type: "Fine dining",
+        price: "$$$$"
+    },
+    {
+        id: 3,
+        name: "Whitebones",
+        address: "Johtokatu 21",
+        openHrs: "14:00 - 22:00",
+        image: "./images/pic3.png",
+        type: "Buffet",
+        price: "$$"
+    },
+    {
+        id: 4,
+        name: "Quaff",
+        address: "Puistotie 2",
+        openHrs: "16:00 - 24:00",
+        image: "./images/pic4.png",
+        type: "Fast food",
+        price: "$$"
+    },
+    {
+        id: 5,
+        name: "MJ's",
+        address: "Tietokuja 3",
+        openHrs: "14:00 - 22:00",
+        image: "./images/pic5.png",
+        type: "Fine dining",
+        price: "$$$$$"
+    },
+]
